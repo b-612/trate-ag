fb426d955dda065eee02dab990b49758110e5784
b-612 <fromtheorion@yandex.ru>
2019-12-21 05:51:25 +0300

    Добавляет стилизацию для инфо-блока в хедере

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('images', function () {
-    return gulp.src('source/img/*.{png,jpg,svg}')
+    return gulp.src('source/img/*.{png,jpg,svg}', 'source/img/towebp/*')
         .pipe(imagemin([
             imagemin.optipng({optimizationLevel: 3}),
             imagemin.jpegtran({progressive: true}),
